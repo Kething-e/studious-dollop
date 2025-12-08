585af8439cceabe86988b664969b5efac7a11699
Kething-e <148595237+Kething-e@users.noreply.github.com>
2025-12-08 14:49:13 -0500

    actual final update

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 // simple answer dictionary for 12 drawers (case-insensitive)
   const ANSWERS = {
     "1": "CTF{us3_y0ur_bIg_bRAINs}",
-    "2": "CTF{rm1ng_Up_w1tH_jAv4_000AXPNPN0i}",
-    "3": "CTF{I_hATE_CinDErEllA",
+    "2": "CTF{w4rm1ng_Up_w1tH_jAv4_000AXPNPN0i}",
+    "3": "CTF{I_hATE_CinDErEllA}",
     "4": "CTF{H0tter_thAn_H0T}",
     "5": "CTF{@sk_th3_1nt3rn_b64c4705}",
     "6": "CTF{t1m3m@ch1n3_b476ca06}",
